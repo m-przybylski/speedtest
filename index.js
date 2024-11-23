039e5d30e684256e7144531ba99bf183ae10f211
Maciej Przybylski <m-przybylski@hotmail.com>
2024-11-23 17:15:39 +0100

    Make it possible to start from different location

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
-import { spawn } from 'node:child_process'
-import { insertReading } from './database.js';
-import { parseResult } from './result-parser.js';
+import { spawn } from "node:child_process";
+import { insertReading } from "./database.js";
+import { parseResult } from "./result-parser.js";
+import { fileURLToPath } from "url";
+import { resolve, dirname } from "node:path";
 
-const acceptLicense = '--accept-license'
-const acceptGdpr = '--accept-gdpr'
+const acceptLicense = "--accept-license";
+const acceptGdpr = "--accept-gdpr";
 
-const speedTest = spawn('./bin/speedtest', [acceptLicense, acceptGdpr, '-f', 'json'])
+const speedTest = spawn(
+  resolve(dirname(fileURLToPath(import.meta.url)), "bin", "speedtest"),
+  [acceptLicense, acceptGdpr, "-f", "json"]
+);
 
-const startTime = Date.now()
+const startTime = Date.now();
 
-speedTest.stdout.setEncoding('utf-8')
-speedTest.stdout.on('data', async (response) => {
+speedTest.stdout.setEncoding("utf-8");
+speedTest.stdout.on("data", async (response) => {
   try {
-    const result = JSON.parse(response)
-    const parsedResult = parseResult(result)
-    const inserted = await insertReading({...parsedResult, response, startTime})
-    console.log(`REcording ID: `, inserted)
-    console.log('New reading stored!')
+    const result = JSON.parse(response);
+    const parsedResult = parseResult(result);
+    const inserted = await insertReading({
+      ...parsedResult,
+      response,
+      startTime,
+    });
+    console.log("New reading stored!", inserted.id);
   } catch (ex) {
-    console.error('error', ex)
+    console.error("error", ex);
     process.exit(1);
   }
-})
+});
